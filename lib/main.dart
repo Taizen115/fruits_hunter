35ecf8c1c300ee9e0bf4fb30a5bfcb75b923e19b
taizen115 <taizen115@gmail.com>
2023-11-22 14:30:41 +0900

    2023.11.22 new

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
